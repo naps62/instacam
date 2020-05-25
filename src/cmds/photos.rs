9935cbf95926190cdfef182ca6487ace1e403791
Miguel Palhas <mpalhas@gmail.com>
2020-05-25 22:13:35 +0100

    saving rgb photo

--- src/cmds/photos.rs
+++ src/cmds/photos.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
+use std::ptr::null_mut;
 use std::slice;
 
 use ffmpeg4_ffi::sys;
@@ -53,25 +54,73 @@ pub unsafe fn debug_frame(ctx: &DecoderCtx) {
 
 pub unsafe fn save_gray_frame(ctx: &DecoderCtx) {
     let frame = *ctx.frame;
+    let codec_ctx = *ctx.codec_ctx;
     let number = (*ctx.codec_ctx).frame_number;
 
     let name = format!("frames/{}.pmg", number);
 
     let width = frame.width;
     let height = frame.height;
-    let linesize = frame.linesize[0];
-    let gray_channel = slice::from_raw_parts(frame.data[0], (width * linesize) as usize);
 
+    let format = sys::AVPixelFormat_AV_PIX_FMT_RGB24;
+    let num_bytes = sys::avpicture_get_size(format, codec_ctx.width, codec_ctx.height) as usize;
+    let buffer = vec![0u8; num_bytes];
+
+    let sws_ctx = sys::sws_getContext(
+        codec_ctx.width,
+        codec_ctx.height,
+        codec_ctx.pix_fmt,
+        codec_ctx.width,
+        codec_ctx.height,
+        format,
+        sys::SWS_BILINEAR as i32,
+        null_mut(),
+        null_mut(),
+        null_mut(),
+    );
+    let rgb_frame = sys::av_frame_alloc();
+
+    sys::avpicture_fill(
+        rgb_frame as *mut sys::AVPicture,
+        buffer.as_ptr(),
+        format,
+        codec_ctx.width,
+        codec_ctx.height,
+    );
+
+    sys::sws_scale(
+        sws_ctx,
+        frame.data.as_ptr() as *const *const u8,
+        frame.linesize.as_ptr() as *const i32,
+        0,
+        codec_ctx.height,
+        (*rgb_frame).data.as_ptr() as *const *mut u8,
+        (*rgb_frame).linesize.as_ptr() as *const i32,
+    );
+
+    let linesize = (*rgb_frame).linesize[0];
+
+    println!(
+        "width: {}\nheight: {}\nlinesize: {}\nheight*linesize: {}\nbyte size: {}",
+        width,
+        height,
+        linesize,
+        height * linesize,
+        num_bytes
+    );
     println!("Saving frame {} into {}", number, name);
+    println!("{:?}", (*rgb_frame).linesize);
 
     let mut file = File::create(name).unwrap();
-    write!(file, "P5\n{} {}\n{}\n", width, height, 255).unwrap();
+    write!(file, "P6\n{} {}\n{}\n", width, height, 255).unwrap();
+
+    let data = slice::from_raw_parts((*rgb_frame).data[0], (height * linesize * 3) as usize);
 
     for i in 0..height {
         let start = (linesize * i) as usize;
-        let end = start + width as usize;
+        let end = start + (width * 3) as usize;
 
-        let line = &gray_channel[start..end];
+        let line = &data[start..end];
 
         file.write_all(line).unwrap();
     }
